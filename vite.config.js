4d30a001606ba49e0f9d7d79883b00e26e0ebdca
santosdlaysa <santosdlaysa@gmail.com>
2023-09-29 13:36:52 -0400

    configuração do vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/lay-coffe/' : '/'
+
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/lay-coffe/' : '/',
+};
